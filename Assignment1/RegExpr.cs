ed8fa754ecd92dc8e7d9ebef35cc3f8590825177
yovitus <vime@itu.dk>
2022-09-09 13:57:32 +0200

    start regex

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -2,9 +2,18 @@ namespace Assignment1;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines){
+        
+    }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(string resolutions) {
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+    }
+
+    public static IEnumerable<string> InnerText(string html, string tag) {
+        
+    }
+
+    IEnumerable<(Uri url, string title)> Urls(string html);
+
 }
