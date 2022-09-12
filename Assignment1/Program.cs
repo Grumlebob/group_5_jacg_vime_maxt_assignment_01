b4949d31309d37c49f227755dc34acb2029abf2d
yovitus <vime@itu.dk>
2022-09-12 17:13:41 +0200

    finish 2nd regex method

---
+++ Assignment1/Program.cs
@@ -0,0 +1,11 @@
+namespace Assignment1;
+
+public class Program
+{
+
+    public static void Main(string[] arg)
+    {
+        Console.WriteLine(RegExpr.Resolution(Console.ReadLine()));
+    }
+
+}
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,19 +1,35 @@
 namespace Assignment1;
-
+using System.Text.RegularExpressions;
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines){
+    
+    // public static IEnumerable<string> SplitLine(IEnumerable<string> lines){
         
-    }
+    // }
 
     public static IEnumerable<(int width, int height)> Resolution(string resolutions) {
-
-    }
-
-    public static IEnumerable<string> InnerText(string html, string tag) {
         
+        var regex = new Regex("(?<width>[0-9]+)x(?<height>[0-9]+)");
+
+        var matches = regex.Matches(resolutions);
+
+        var output = new List<(int width, int height)>();
+        
+        foreach(Match match in matches) {
+            var groups = match.Groups;
+
+            output.Add((Int32.Parse(groups["width"].Value), Int32.Parse(groups["height"].Value)));
+            
+        }
+
+        return output;
     }
 
-    IEnumerable<(Uri url, string title)> Urls(string html);
+//   {  public static IEnumerable<string> InnerText(string html, string tag) {
+        
+//     }
 
+//     IEnumerable<(Uri url, string title)> Urls(string html);
+
+// }
 }
